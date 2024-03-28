504e1c2973f328468f3aa682f0b062e196e14a13
SammyUEO <samuel.pop@emanuel.ro>
2024-03-28 21:43:06 +0200

    pregatire teren

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello!');
+let sunRadius = 30;
+let planetRadii = [8, 12, 16, 20, 24]; 
+let distances = [80, 120, 160, 200, 240]; 
+let ovalFactor = 0.6; 
+let rotationSpeeds = [0.5, 0.7, 0.6, 0.4, 0.3];
+let planetColors = ["red", "blue", "green", "brown", "purple"];
